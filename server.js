4c3f9738994c851676916c680bf0d7f1fd904757
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2022-11-03 22:35:15 +0100

    modified server.js

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const express= require("express");
 const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+require("dotenv").config()
 
 
-const db = require("./middleware/db");
 
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
-
-db.connectToMongoDB();
+const {authUser}= require("./middleware/auth")
+const userRoute= require("./routes/user")
+const blogRoute= require("./routes/homeRouter")
+const createRoute= require("./routes/createRouter")
 
 
 PORT= process.env.PORT;
 
+const db = require("./middleware/db");
 
+
+const app = express()
+
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+db.connectToMongoDB();
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+
+
+
+
+app.use('/', blogRoute);
+app.use('/', userRoute);
+app.use('/write', authUser, (req,res) => res.render("createBlog"));
 
 
 
