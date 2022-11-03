ff111ea8fae5fec8bfd18ba6170fa1114a67168e
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2022-11-03 22:33:35 +0100

    add static methods for signup and login

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
   id: ObjectId,
 
   first_name: {
@@ -36,5 +38,50 @@ const UserSchema = new Schema({
 });
 
 
+userSchema.statics.signup = async function(first_name, last_name, username, email, password) {
 
-module.exports = new mongoose.model("User", UserSchema);
+  if (!first_name || !last_name || !username || !email || !password) {
+    throw Error('All fields must be completed!')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email is not valid')
+  }
+  // if (!validator.isStrongPassword(password)) {
+  //   throw Error('Password is not strong enough. Use a combination of uppercase, lowercase and special characters')
+  // }
+
+  const isinUse = await this.findOne({ email })
+
+  if (isinUse) {
+    throw Error('Email already exists')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await this.create({ first_name, last_name, username, email, password: hashedPassword })
+
+  return user
+}
+
+// static login method
+userSchema.statics.login = async function(username, password) {
+
+  if (!username || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({ username })
+  if (!user) {
+    throw Error('Incorrect username')
+  }
+
+  const matchPassword = await bcrypt.compare(password, user.password)
+  if (!matchPassword) {
+    throw Error('Incorrect password')
+  }
+
+  return user
+}
+
+module.exports = mongoose.model('User', userSchema)
