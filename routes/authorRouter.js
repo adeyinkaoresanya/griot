d0704ec86d84d7c517f10680d4b0a996f4a15fdc
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2022-11-05 07:34:06 +0100

    implement author name

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const blog = require("../models/blogModel");
 
-router.get("/", async (req, res) => {
+router.get("/author", async (req, res) => {
   const allBlogs = await blog.find().populate('author').exec();
-  res.render("index", { blogs: allBlogs });
+  // res.json({ blogs: allBlogs });
+ res.render("authorBlogs", { blogs: allBlogs });
 });
   module.exports = router;
